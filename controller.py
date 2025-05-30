a607c56aac96cdab4c4a14ded9036a952fc9823b
rickert156 <rickert156@rambler.ru>
2025-05-30 12:37:39 +0400

    update main module

--- controller.py
+++ controller.py
@@ -1,4 +1,6 @@
 from modules.colors import RED, RESET, GREEN
+from modules.helper import helper
+from modules.request import initRequest
 from modules.miniTools import (
                ListServersCSV,
                ListServersTXT
@@ -7,20 +9,26 @@ import sys
 
 def ControllerVDS():
     params = sys.argv
+    """Произвольная команда"""
     if len(params) == 4 and params[1] == "--command":
         if '.csv' in params[2] or '.txt' in params[2]:
             servers = params[2]
             command = params[3]
-            print(command)
             if '.csv' in servers:
                 list_servers = ListServersCSV(doc=servers)
             if '.txt' in servers:
                 list_servers = ListServersTXT(doc=servers)
+
+            initRequest(list_servers=list_servers, command=command)
     
     elif len(params) == 3 and params[1] == "--command":
         print(
                 f"{RED}Необходимо добавить файл и ввести "
                 f"команду с переменной в скобках {{server}} !{RESET}")
+        print(helper())
+
+    elif len(params) == 1:
+        print(helper())
 
 ControllerVDS()
 
